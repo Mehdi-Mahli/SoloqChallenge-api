e26061e7c22b8b6c37680b5f7848cbd2381b7863
Mehdi-Mahli <bxl-mehdi@hotmail.com>
2026-02-14 15:18:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ let cache = {
   lastUpdate: 0
 };
 
-const CACHE_DURATION = 5 * 60 * 1000; // 15 minutes
+const CACHE_DURATION = 5 * 60 * 1000; // 5 minutes
 
 /* ========================= */
 /* VALEUR DES RANKS */
@@ -245,3 +245,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Serveur lancé sur le port ${PORT}`);
 });
+
